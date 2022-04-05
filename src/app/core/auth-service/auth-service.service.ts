6d8746d8acbc0bdeeaa324f63bb51c2914aa7b01
Inana Eftimova <eftimovainana@gmail.com>
2022-04-05 12:39:45 +0300

    Added authentication

--- src/app/core/auth-service/auth-service.service.ts
+++ src/app/core/auth-service/auth-service.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut,
+  onAuthStateChanged  
+} from "firebase/auth";
+
+let uid: any = null;
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthServiceService {
 
+export class AuthServiceService {
   constructor() { }
 
   register(email: string, password: string){
@@ -18,7 +26,39 @@ export class AuthServiceService {
   login(email: string, password: string){
     const auth = getAuth();
     signInWithEmailAndPassword(auth, email, password)
-    .then(data => console.log(data))
+    .then(data => {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+           uid = user.uid;
+        }
+      })
+    })
     .catch(err => console.log(err));
+  }
+
+  logout(){
+    // const auth = getAuth();
+    // signOut(auth)
+    // .then((data) => {
+    //   uid = null;
+    // })
+    // .catch(err => console.log(err));
+    uid = null;
+  }
+
+  getUId(){
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+         uid = user.uid;
+         console.log(user);
+      }
+    })
+
+    return uid;
+  }
+
+  isAuthenticated(): boolean{
+    return uid != null;
   }
 }
